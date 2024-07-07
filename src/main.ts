b1126c058fda1433d8f96cdc10ffeab6f26d6f49
user530 <m.timarsuev@gmail.com>
2024-07-08 00:45:01 +0300

    Changed user service to the microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 3000,
+    }
+  });
+  await app.listen();
 }
 bootstrap();
--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getPlaceholder() {
-    return { id: 1, name: 'John Doe' };
+    return { id: 1, name: 'Bob Ross' };
   }
 }
